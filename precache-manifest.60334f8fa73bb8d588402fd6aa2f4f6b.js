baebd3a62deca680f1e16a06c3cb1b2a49339065
Carlos Gasperi <cgasperi90@gmail.com>
2020-06-24 15:33:22 -0400

    Updates

---
+++ precache-manifest.60334f8fa73bb8d588402fd6aa2f4f6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1478e7a15bea246f8c0b7db331503ddf",
+    "url": "/ClickyGameReact/index.html"
+  },
+  {
+    "revision": "e352a066a1894f71655b",
+    "url": "/ClickyGameReact/static/css/main.f55b6c08.chunk.css"
+  },
+  {
+    "revision": "f25c31f78b542785a051",
+    "url": "/ClickyGameReact/static/js/2.0064d290.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGameReact/static/js/2.0064d290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e352a066a1894f71655b",
+    "url": "/ClickyGameReact/static/js/main.a2d7c800.chunk.js"
+  },
+  {
+    "revision": "dcb87052adbc436c0d4d",
+    "url": "/ClickyGameReact/static/js/runtime-main.14d96f70.js"
+  }
+]);
